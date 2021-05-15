926972b0e17afff37b4a7e1e2f10fedbdfc59c46
Aline Richter <linebio.r@gmail.com>
2021-05-14 22:46:51 -0300

    visualization of HD, improvement of figures

--- R/S_HD_bfly.R
+++ R/S_HD_bfly.R
@@ -1,4 +1,3 @@
-
 # visual evaluation of hidden diversity (HD) to multiple dimension of biodiversity --------
 
 ### calculating the hidden diversity
@@ -72,13 +71,18 @@ res.HD.bfly <- data.frame(H.TD = (HD.bfly$SES.PDobs$ntaxa - list_mean_sd$sesPD.e
                           H.MFD = (HD.bfly$SES.MFDobs$mpd.obs.z - list_mean_sd$sesMFD.est$mean[,"mpd.obs.z"])/list_mean_sd$sesMFD.est$sd[,"mpd.obs.z"])
 res.HD.bfly$Strata <- ifelse(substr(rownames(res.HD.bfly), 1, 1) == "C", "Canopy", 
                               "Understory")
+res.HD.bfly$Month <- ifelse(substr(rownames(res.HD.bfly), 6, 6) == "N", "November", 
+                             ifelse(substr(rownames(res.HD.bfly), 6, 6) == "D", "December",
+                                    ifelse(substr(rownames(res.HD.bfly), 6, 6) == "J", "January",
+                                           ifelse(substr(rownames(res.HD.bfly), 6, 6) == "F", "February", "March"))))
+res.HD.bfly$SU <- substr(rownames(res.HD.bfly), 5, 5) 
 
 # converting in a data frame
 df.hd <- data.frame(HD.values = c(res.HD.bfly$H.TD, res.HD.bfly$H.PD, res.HD.bfly$H.FD,
                                   res.HD.bfly$H.MPDi, res.HD.bfly$H.MPD, 
                                   res.HD.bfly$H.MFDi, res.HD.bfly$H.MFD), 
                     Div.names = rep(colnames(res.HD.bfly[, 1:7]), each = nrow(res.HD.bfly)),
-                    Strata = rep(res.HD.bfly$Stratum, 7))
+                    Strata = rep(res.HD.bfly$Strata, 7))
 df.hd$Div.names1 <- factor(df.hd$Div.names, levels = c("H.TD", "H.PD", "H.FD",
                                                        "H.MPDi", "H.MFDi", "H.MPD", "H.MFD"))
 
@@ -90,64 +94,49 @@ p.hd <- ggplot(data = df.hd, aes(y = HD.values, x = Div.names1,
   geom_boxplot() + scale_color_viridis_d(option = "A") +
   scale_fill_viridis_d(option = "A", alpha = 0.5) +
   geom_hline(yintercept = 0, linetype = 2, color = "firebrick1") +
-  labs(x = "Diversity Measures", y = "Hidden Diversity") 
+  labs(x = "Diversity Measures", y = "Hidden Diversity") +
+  facet_wrap(~Div.names1)
 p.hd
 
 cowplot::save_plot(here::here("output", "figures", "Fig1_hdbfly.png"), p.hd,
                    base_width = 8)
 
 # testing if the HD differs between strata
-mod.td <- lm(res.HD.bfly$H.TD ~ res.HD.bfly$Strata)
+library(lme4)
+library(lmerTest)
+
+str(res.HD.bfly)
+mod.td <- lmer(H.TD ~ Strata + (1|Month)  + (1|SU), data = res.HD.bfly)
 summary(mod.td)
 
-mod.pd <- lm(res.HD.bfly$H.PD ~ res.HD.bfly$Strata)
+mod.pd <- lmer(H.PD ~ Strata + (1|Month)  + (1|SU), data = res.HD.bfly)
 summary(mod.pd)
 
-mod.pd1 <- lm(HD.bfly$SES.PDobs$pd.obs.z ~ list_mean_sd$sesPD.est$mean[,6])
-summary(mod.pd1)
-
-plot(HD.bfly$SES.PDobs$pd.obs.z ~ list_mean_sd$sesPD.est$mean[,6])
-abline(a = coef(mod.pd1)[1], b = coef(mod.pd1)[2])
-abline(a = 0, b = 1, col = "red")
-
-mod.fd <- lm(res.HD.bfly$H.FD ~ res.HD.bfly$Strata)
+mod.fd <- lmer(H.FD ~ Strata + (1|Month)  + (1|SU), data = res.HD.bfly)
 summary(mod.fd)
 
-mod.mpdi <- lm(res.HD.bfly$H.MPDi ~ res.HD.bfly$Strata)
+mod.mpdi <- lmer(H.MPDi ~ Strata + (1|Month)  + (1|SU), data = res.HD.bfly)
 summary(mod.mpdi)
 
-mod.mfdi <- lm(res.HD.bfly$H.MFDi ~ res.HD.bfly$Strata)
+mod.mfdi <- lmer(H.MFDi ~ Strata + (1|Month)  + (1|SU), data = res.HD.bfly)
 summary(mod.mfdi)
 
-mod.mpd <- lm(res.HD.bfly$H.MPD ~ res.HD.bfly$Strata)
+mod.mpd <- lmer(H.MPD ~ Strata + (1|Month)  + (1|SU), data = res.HD.bfly)
 summary(mod.mpd)
 
-mod.mfd <- lm(res.HD.bfly$H.MFD ~ res.HD.bfly$Strata)
+mod.mfd <- lmer(H.MFD ~ Strata + (1|Month)  + (1|SU), data = res.HD.bfly)
 summary(mod.mfd)
 
-out.lm <- rbind(summary(mod.td)[[4]], summary(mod.pd)[[4]],
-                summary(mod.fd)[[4]], summary(mod.mpdi)[[4]],
-                summary(mod.mpd)[[4]], summary(mod.mfdi)[[4]],
-                summary(mod.mfd)[[4]])
-rownames(out.lm) <- rep(colnames(res.HD.bfly[, 1:7]), each = 2)
+out.lmer <- rbind(summary(mod.td)$coefficients, summary(mod.pd)$coefficients,
+                  summary(mod.fd)$coefficients, summary(mod.mpdi)$coefficients,
+                  summary(mod.mpd)$coefficients, summary(mod.mfdi)$coefficients,
+                  summary(mod.mfd)$coefficients)
 
-round(out.lm, 3)
 
-# correlation between TD and SES.PD/FD
-mod.td.pd <- lm(res.HD.bfly$H.TD ~ res.HD.bfly$H.PD)
-summary(mod.td.pd)
-cor.test(res.HD.bfly$H.TD, res.HD.bfly$H.PD)
-plot(H.TD ~ H.PD, data = res.HD.bfly)
-abline(mod.td.pd)
+rownames(out.lmer) <- rep(colnames(res.HD.bfly[, 1:7]), each = 2)
 
-mod.td.fd <- lm(res.HD.bfly$H.TD ~ res.HD.bfly$H.FD)
-summary(mod.td.fd)
-cor.test(res.HD.bfly$H.TD, res.HD.bfly$H.FD)
-plot(res.HD.bfly$H.TD ~res.HD.bfly$H.FD)
-abline(mod.td.fd)
-
-out.cor <- rbind(summary(mod.td.pd)[[4]], summary(mod.td.fd)[[4]])
-
+round(out.lmer, 3)
+write.table(out.lmer, here::here("output", "mod_lmer.txt"))
 
 
 # visualization of the hidden diversity in another way --------------------
@@ -163,76 +152,59 @@ df.HD <- data.frame(D.obs = c(HD.bfly$SES.PDobs$ntaxa, HD.bfly$SES.PDobs$pd.obs.
                                list_mean_sd$sesFD.est$sd[,6], list_mean_sd$sesMPDi.est$sd[,6],
                                list_mean_sd$sesMFDi.est$sd[,6], list_mean_sd$sesMPD.est$sd[,6],
                                list_mean_sd$sesMFD.est$sd[,6]),
-                    Div.name = rep(c("SES.TD","SES.PD", "SES.FD", "SES.MPDi", "SES.MFDi",
+                    Div.name = rep(c("TD","SES.PD", "SES.FD", "SES.MPDi", "SES.MFDi",
                                      "SES.MPD", "SES.MFD"), each = nrow(HD.bfly$SES.PDobs)), 
                     Strata = rep(substr(rownames(HD.bfly$SES.PDobs),1,1), 7))
 df.HD$Strata <- ifelse(df.HD$Strata == "C", "Canopy", "Understory") 
 df.HD$HD <- (df.HD$D.obs - df.HD$D.est)/df.HD$SD.est
-df.HD$Div.name <- factor(df.HD$Div.name, levels = c("SES.TD","SES.PD", "SES.FD", "SES.MPDi", "SES.MFDi",
-                                                    "SES.MPD", "SES.MFD"))
+df.HD$Div.name <- factor(df.HD$Div.name, levels = c("TD","SES.PD", "SES.MPDi", "SES.MPD", "SES.FD", 
+                                                    "SES.MFDi", "SES.MFD"))
 
-p.obs_est <- ggplot(df.HD, aes(x = D.est, y = D.obs, colour = Strata), alpha, .5, size = 4) +
+p.obs_est <- ggplot(df.HD, aes(x = D.obs, y = D.est, colour = Strata), alpha, .5, size = 4) +
   geom_point(alpha = .5) + scale_color_viridis_d(option = "A") +
   geom_vline(xintercept = 0, linetype = "dashed", color = "lightblue") +
   geom_hline(yintercept = 0, linetype = "dashed", color = "lightblue") +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "firebrick1") +
-  facet_wrap(~ Div.name, scales = "free") + theme(legend.position = "none")
+  facet_wrap(~ Div.name, nrow = 1) + theme(legend.position = "none") +
+  labs(y = "Estimated Diversity", x = "Observed Diversity", tag = "b)")
 p.obs_est
 
-p.hd <- ggplot(df.HD, aes(x = Div.name, y = (HD*-1), colour = Strata, fill = Strata)) +
+library(ggplot2)
+colnames(df.HD)
+#HD.bfly <- readRDS(here::here("output", "HD_bfly.rds"))
+
+p.div.pat <- ggplot(df.HD, aes(x = Div.name, y = D.obs, colour = Strata, fill = Strata)) +
+  geom_boxplot(alpha = 0.5) + ylim(-4, 12) + scale_color_viridis_d(option = "A") +
+  scale_fill_viridis_d(option = "A") + 
+  geom_hline(yintercept = 0, linetype = "dashed", color = "firebrick1") +
+  labs(y = "Observed diversity", x = " ", tag = "a)") +
+  theme(legend.position = "none")
+p.div.pat
+
+p.div.pat1 <- ggplot(df.HD, aes(x = Div.name, y = D.est, colour = Strata, fill = Strata)) +
+  geom_boxplot(alpha = 0.5) + ylim(-4, 12) + scale_color_viridis_d(option = "A") +
+  scale_fill_viridis_d(option = "A") + 
+  geom_hline(yintercept = 0, linetype = "dashed", color = "firebrick1") +
+  labs(y = "Estimated diversity", x = " ", tag = "b)") +
+  theme(legend.position = "none")
+p.div.pat1
+
+cowplot::save_plot(here::here("output","figures", "Fig.S3_obs_est.png"),
+                   cowplot::plot_grid(p.div.pat, p.div.pat1),
+                   base_width = 10)
+
+
+p.hd <- ggplot(df.HD, aes(x = Div.name, y = HD, colour = Strata, fill = Strata)) +
   geom_boxplot(alpha = .5) + facet_wrap(~ Div.name, nrow = 1, scales = "free_x") +
   scale_color_viridis_d(option = "A") + scale_fill_viridis_d(option = "A") +
   geom_hline(yintercept = 0, linetype = "dashed", color = "firebrick1") +
-  theme(legend.position = "none")
+  theme(legend.position = "none") + labs(y = "Hidden Diversity", tag = "a)")
+p.hd
 
-plot1 <- cowplot::plot_grid(p.obs_est, p.hd, ncol = 1)
-cowplot::save_plot(here::here("output", "figures", "Figx_HD.png"),
+plot1 <- cowplot::plot_grid(p.hd, p.obs_est,  ncol = 1)
+plot1
+
+cowplot::save_plot(here::here("output", "figures", "Fig3_HD.png"),
                    plot1, base_width = 10, base_height = 6)
 
 
-
-# Testing if the Estimated Diversity can predict the observed diversity --------
-
-mod1 <- lm(HD.bfly$SES.PDobs$ntaxa ~ list_mean_sd$sesPD.est$mean[,1])
-summary(mod1)
-residuals(mod1)
-
-mod2 <- lm(HD.bfly$SES.FDobs$pd.obs.z ~ list_mean_sd$sesFD.est$mean[,6])
-summary(mod2)
-residuals(mod2)
-cor.test(residuals(mod2), res.HD.bfly$H.FD)
-
-mod3 <- lm(HD.bfly$SES.MPDiobs$mpd.obs.z ~ list_mean_sd$sesMPDi.est$mean[,6])
-summary(mod3)
-cor.test(residuals(mod3), res.HD.bfly$H.MPDi)
-
-mod4 <- lm(HD.bfly$SES.MPDobs$mpd.obs.z ~ list_mean_sd$sesMPD.est$mean[,6])
-summary(mod4)
-cor.test(residuals(mod4), res.HD.bfly$H.MPD)
-
-mod5 <- lm(HD.bfly$SES.MFDiobs$mpd.obs.z ~ list_mean_sd$sesMFDi.est$mean[,6])
-summary(mod5)
-cor.test(residuals(mod5), res.HD.bfly$H.MFDi)
-
-mod6 <- lm(HD.bfly$SES.MFDobs$mpd.obs.z ~ list_mean_sd$sesMFD.est$mean[,6])
-summary(mod6)
-cor.test(residuals(mod6), res.HD.bfly$H.MFD)
-plot(residuals(mod6), res.HD.bfly$H.MFD)
-
-
-
-obs.pd <- HD.bfly$SES.PDobs[,6]
-est.pd <- list_mean_sd$sesPD.est$mean[,6]
-
-sum(obs.pd > est.pd)
-ts.o <- obs.pd[which(obs.pd < est.pd)]
-ts.e <- est.pd[which(obs.pd < est.pd)]
-
-plot(ts.e, ts.o)  
-abline(a = 0, b = 1)
-a = 0
-b = 1
-ts.e.pred <- (a + b*ts.e)
-which(max(ts.o - ts.e.pred) == ts.o - ts.e.pred)
-
-res.HD.bfly[which(rownames(res.HD.bfly) == "C2NF5D"),]
